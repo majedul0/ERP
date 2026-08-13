9bbc0a7241f896bd52b7ec4646eeaa1954f60d7d
majedul0 <islam22205101253@diu.edu.bd>
2026-08-13 13:10:17 +0600

    u

--- resources/js/modules/company/layouts/company-layout.tsx
+++ resources/js/modules/company/layouts/company-layout.tsx
@@ -1,4 +1,5 @@
 ﻿import CompanyHeader from '../components/company-header';
+import SubscriptionBanner from '../components/subscription-banner';
 import { useCompanyBrand } from '../hooks/use-company-brand';
 
 /**
@@ -16,6 +17,7 @@ export default function CompanyLayout({
         <div className="flex min-h-screen flex-col bg-coffee-50/40">
             <div className="print:hidden">
                 <CompanyHeader brand={brand} />
+                <SubscriptionBanner />
             </div>
             <main className="mx-auto w-full max-w-[1600px] flex-1 px-4 py-5 lg:px-6">
                 {children}
