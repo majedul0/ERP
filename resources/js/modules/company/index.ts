a43e5f6ebd6c42ce3b7c76c12efcd216c2451cdc
majedul0 <islam22205101253@diu.edu.bd>
2026-08-18 12:12:32 +0600

    u

--- resources/js/modules/company/index.ts
+++ resources/js/modules/company/index.ts
@@ -8,6 +8,7 @@ export { default as CompanyLogo } from './components/company-logo';
 export { default as StarBackdrop } from './components/star-backdrop';
 export { useCan } from './hooks/use-can';
 export { useCompanyBrand } from './hooks/use-company-brand';
+export { useCompanyTheme } from './hooks/use-company-theme';
 export { default as CompanyLayout } from './layouts/company-layout';
 export { companyNavItems, companyQuickActions } from './nav-items';
 export type { CompanyNavItem } from './nav-items';
